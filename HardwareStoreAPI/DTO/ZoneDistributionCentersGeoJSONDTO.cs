c7b624c11b1df81c7d98425dafb637bb5a630ce8
Brian Wiggins <irdetection@hotmail.com>
2021-10-21 07:00:02 -0400

    Changed Zone Distribution DTO
    Created GeoJSON Zone DistributionDTO

---
+++ HardwareStoreAPI/DTO/ZoneDistributionCentersGeoJSONDTO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetTopologySuite.Geometries;
+
+namespace HardwareStoreAPI.DTOs
+{
+    public class ZoneDistributionCentersGeoJSONDTO
+    {
+        public string type { get; set; } = "FeatureCollection";
+        public Feature[] features { get; set; }
+    }
+
+    public class Feature 
+    {
+        public string type { get; set; } = "Feature";
+        public Properties properties { get; set; }
+        public Geometry geometry { get; set; }
+    }
+
+    public class Properties
+    {
+        public long ZoneDistributionCenterID { get; set; } 
+        public string ZoneDistributionCenterStreet{ get; set; }
+        public string ZoneDistributionCenterCity { get; set; }
+        public string ZoneDistributionCenterState { get; set; }
+        public string ZoneDistributionCenterZipCode { get; set; }
+        public string ZoneDistributionCenterCountryCode { get; set; }
+        public string ZoneDistributionCenterCode { get; set; }
+        public bool ActiveStatus { get; set; }
+        public long fkDistrictSalesZoneID { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string type { get; set; } = "Point";
+        public decimal[] coordinates { get; set; }
+    }
+
+}
---
+++ HardwareStoreBusinessLogicLibrary/ControllerLogic/DistributionCenter/DistributionCenterBuisnessLogic.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+
+
+namespace HardwareStoreBusinessLogicLibrary.ControllerLogic.DistributionCenter
+{
+    public class DistributionCenterBuisnessLogic
+    {
+        public async Task<ZoneDistributionCentersGeoJSONDTO> CreateGeoJSONDTOfromModel(ZoneDistributionCenters)
+        {
+
+        }
+    }
+}
